bd576be3787cb91772dd7be304b58bdc3b91ef21
zi7fa159 <nazih.belaissa123@gmail.com>
2025-03-08 00:56:08 +0000

    uu

--- next.config.js
+++ next.config.js
@@ -7,6 +7,8 @@ const nextConfig = {
   images: {
     domains: ["images.unsplash.com"],
     unoptimized: process.env.NODE_ENV === "development",
+    deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+    imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
   },
   compress: true,
   poweredByHeader: false,
