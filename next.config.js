5df73e8d4e020c5e5a5016a6281e53548a3be822
zi7fa159 <nazih.belaissa123@gmail.com>
2025-03-08 00:47:48 +0000

    twtw

--- next.config.js
+++ next.config.js
@@ -1,27 +1,28 @@
 /** @type {import('next').NextConfig} */
-const withBundleAnalyzer = require('@next/bundle-analyzer')({
-    enabled: process.env.ANALYZE === 'true',
+const withBundleAnalyzer = require("@next/bundle-analyzer")({
+  enabled: process.env.ANALYZE === "true",
 });
 
 const nextConfig = {
-    images: {
-        domains: ['images.unsplash.com'],
-        unoptimized: process.env.NODE_ENV === 'development'
-    },
-    compress: true,
-    poweredByHeader: false,
-    reactStrictMode: true,
-    swcMinify: true
+  images: {
+    domains: ["images.unsplash.com"],
+    unoptimized: process.env.NODE_ENV === "development",
+  },
+  compress: true,
+  poweredByHeader: false,
+  reactStrictMode: true,
+  swcMinify: true,
+};
 
 if (process.env.NEXT_PUBLIC_TEMPO) {
-    nextConfig["experimental"] = {
-        // NextJS 13.4.8 up to 14.1.3:
-        // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
-        // NextJS 14.1.3 to 14.2.11:
-        swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]]
+  nextConfig["experimental"] = {
+    // NextJS 13.4.8 up to 14.1.3:
+    // swcPlugins: [[require.resolve("tempo-devtools/swc/0.86"), {}]],
+    // NextJS 14.1.3 to 14.2.11:
+    swcPlugins: [[require.resolve("tempo-devtools/swc/0.90"), {}]],
 
-        // NextJS 15+ (Not yet supported, coming soon)
-    }
+    // NextJS 15+ (Not yet supported, coming soon)
+  };
 }
 
 module.exports = withBundleAnalyzer(nextConfig);
